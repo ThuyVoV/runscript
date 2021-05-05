001e7bd975f5d53fc8d0d239349524d434dbe8a0
tino <vovthuy@gmail.com>
2021-05-05 13:40:24 -0700

    Revert "openS"
    This reverts commit b6de9899

--- runscript/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import UploadFileModel, ScriptList, ScriptLog
-
-# Register your models here.
-admin.site.register(UploadFileModel)
-admin.site.register(ScriptList)
-admin.site.register(ScriptLog)
