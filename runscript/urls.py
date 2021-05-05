29f7c358dcafc2e2f8482e4b6f2e5e5c9fbed90c
tino <vovthuy@gmail.com>
2021-05-05 13:20:25 -0700

    openS

---
+++ runscript/urls.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+from . import views
+
+app_name = 'runscript'
+urlpatterns = [
+    path('list/<list_id>/', views.view_and_upload, name='view_and_upload'),
+    path('', views.create_list, name="main"),
+    path('detail/<file_id>/', views.script_detail, name='detail'),
+    path('<file_id>/change/', views.script_change, name='change'),
+    path('<file_id>/change/confirmedit/', views.script_confirm_edit, name='confirm_edit'),
+    path('<file_id>/change/confirmdelete/', views.script_confirm_delete, name='confirm_delete'),
+    path('list/<list_id>/logs/', views.logs, name='logs'),
+    #path('list/<int:pk>/logs/', views.Logs.as_view(), name='logs'),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
