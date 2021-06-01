3180c9e55aa8b799e844c5af862aaf027ed91a9b
tino <vovthuy@gmail.com>
2021-06-01 15:09:41 -0700

    better page access restriction

--- runscript/helper_func/decorators.py
+++ runscript/helper_func/decorators.py
@@ -19,22 +19,35 @@ class AccessCheck(object):
         self.func = func
 
     def __call__(self, *args, **kwargs):
-        try:
-
-            print(self.func.__name__)
-        except Exception:
-
-            print("no name")
-
         script_list = get_list(**kwargs)
-
         if script_list is None:
             return self.func(*args, **kwargs)
         if args[0].user not in script_list.user.all():
             return redirect('runscript:main')
+
+        try:
+            user = args[0].user
+            fun = self.func.__name__
+            need_view = ["view_and_upload", "script_detail"]
+            need_edit = ["script_change", "script_confirm_edit", "script_confirm_delete"]
+            need_log = ["logs"]
+            need_manage = ["manage_user"]
+
+            if fun in need_view:
+                if not user.has_perm(f"runscript.{script_list.owner}_{script_list.list_name}_can_view"):
+                    return redirect('runscript:view_and_upload', script_list.id)
+            elif fun in need_edit:
+                if not user.has_perm(f"runscript.{script_list.owner}_{script_list.list_name}_can_edit"):
+                    return redirect('runscript:view_and_upload', script_list.id)
+            elif fun in need_log:
+                if not user.has_perm(f"runscript.{script_list.owner}_{script_list.list_name}_can_log"):
+                    return redirect('runscript:view_and_upload', script_list.id)
+            elif fun in need_manage:
+                if not user.has_perm(f"runscript.{script_list.owner}_{script_list.list_name}_can_manage_user"):
+                    return redirect('runscript:view_and_upload', script_list.id)
+
+        except AttributeError:
+            print("function no name")
+
         return self.func(*args, **kwargs)
-
-    def check_permission(self, script_list):
-        pass
-
 
